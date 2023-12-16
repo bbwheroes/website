995ef673de159c2ed26a83a97731cdaabb99de17
Jannis Milz <jannis@milz.ch>
2023-12-16 17:51:07 +0100

    Add home link & configure wiki logo link

--- theme.config.jsx
+++ theme.config.jsx
@@ -5,6 +5,7 @@ import { Navbar } from "nextra-theme-docs";
 export default {
   // logo: <Image src="bbwheroes.svg" alt="BBW Heroes Logo" width={100} height={100} />,
   logo: <span>BBW Heroes Wiki</span>,
+  logoLink: "/wiki",
   project: {
     link: "https://github.com/bbwheroes/website/",
   },
