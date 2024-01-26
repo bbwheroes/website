4af7dde67c525d26eaae6597cb7374e9753deecb
Jannis Milz <jannis@milz.ch>
2024-01-26 13:54:16 +0100

    Add target attribute to project github link

--- app/_components/Projects/ProjectRow.jsx
+++ app/_components/Projects/ProjectRow.jsx
@@ -10,7 +10,7 @@ export default function ProjectRow({ project }) {
         <p className="text-lg font-medium">{fullProjectName}</p>
         <span className="text-gray-400">{project.task_name}</span>
       </div>
-      <a href={`https://github.com/bbwheroes/${fullProjectName}`} className="flex aspect-square h-10 items-center justify-center rounded-full bg-gray-700 text-gray-300 duration-100 hover:bg-gray-600">
+      <a href={`https://github.com/bbwheroes/${fullProjectName}`} target="_blank" className="flex aspect-square h-10 items-center justify-center rounded-full bg-gray-700 text-gray-300 duration-100 hover:bg-gray-600">
         <FaGithub className="text-lg" />
       </a>
     </li>
