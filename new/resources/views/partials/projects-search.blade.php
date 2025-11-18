dc2e04cd0e632ba976ad184aba011d289458ca4a
Jannis Milz <jannis.milz@bsi-software.com>
2025-11-18 12:25:06 +0100

    feat: Max width of project boxes and github link button

--- new/resources/views/partials/projects-search.blade.php
+++ new/resources/views/partials/projects-search.blade.php
@@ -52,23 +52,19 @@
     </p>
 
     <!-- Projects List -->
-    <ul class="flex flex-col gap-4">
+    <ul class="mx-auto flex max-w-4xl flex-col gap-3">
         <template x-for="project in searchedProjects" :key="project.id">
-            <li class="rounded-lg border border-gray-700 bg-gray-800 p-4 transition-colors hover:border-bbw-400">
-                <a :href="project.html_url" target="_blank" class="block">
-                    <div class="mb-2 flex items-start justify-between">
-                        <div class="flex-1">
-                            <h3 class="mb-1 font-mono text-lg text-white">
-                                <span class="text-bbw-400" x-text="project.module"></span>-<span x-text="project.teacher"></span>-<span x-text="project.slugified_task_name"></span>-<span x-text="project.username"></span>
-                            </h3>
-                            <p class="text-gray-300" x-text="project.task_name"></p>
-                        </div>
-                        <svg class="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-                            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                        </svg>
-                    </div>
-                    <p class="text-sm text-gray-400" x-text="project.description || 'No description'"></p>
+            <li class="flex items-center justify-between rounded-lg border border-gray-700 bg-gray-800 p-4 shadow-sm shadow-gray-700 transition-colors hover:border-bbw-400">
+                <div class="flex-1 min-w-0">
+                    <h3 class="mb-1 font-mono text-base text-white">
+                        <span class="text-bbw-400" x-text="project.module"></span>-<span x-text="project.teacher"></span>-<span x-text="project.slugified_task_name"></span>-<span x-text="project.username"></span>
+                    </h3>
+                    <p class="text-sm text-gray-400" x-text="project.task_name"></p>
+                </div>
+                <a :href="project.html_url" target="_blank" class="ml-4 flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-gray-700 text-gray-300 transition-colors duration-100 hover:bg-gray-600">
+                    <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                    </svg>
                 </a>
             </li>
         </template>
